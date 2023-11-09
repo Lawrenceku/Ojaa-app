19222f4057110b72904bda5be41b0a443e855670
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-11-09 11:07:44 +0100

    404 error page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path='*' element={<Login/>}/>
+        <Route element={<Login/>}/>
         <Route path='/home' element={<Home/>}/>
       </Routes>
     </Router>
